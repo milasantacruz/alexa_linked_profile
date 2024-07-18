2088fe57d375303ea3e99c1ba4c5a26351c97b6b
camila santacuz <milalovechaton@gmail.com>
2024-07-17 23:40:09 -0300

    agregue funcion requestInfoHandler a index.js

--- lambda/index.js
+++ lambda/index.js
@@ -19,6 +19,64 @@ const LaunchRequestHandler = {
     }
 };
 
+function getResolvedSlotIDValue (request, slotName) {
+    const slot = request.intent.slots[slotName]
+  
+    if (slot &&
+      slot.value &&
+      slot.resolutions &&
+      slot.resolutions.resolutionsPerAuthority &&
+      slot.resolutions.resolutionsPerAuthority[0] &&
+      slot.resolutions.resolutionsPerAuthority[0].values &&
+      slot.resolutions.resolutionsPerAuthority[0].values[0] &&
+      slot.resolutions.resolutionsPerAuthority[0].values[0].value &&
+      slot.resolutions.resolutionsPerAuthority[0].values[0].value.name) {
+  
+      return slot.resolutions.resolutionsPerAuthority[0].values[0].value.id
+    }
+    return null
+  }
+  
+
+const RequestInfoHandler={
+    canHandle(handlerInput){
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'RequestInfoIntent';
+    },
+
+    handle (handlerInput) {
+        const request = handlerInput.requestEnvelope.request
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes()
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes()
+        const repromptOutput = requestAttributes.t('FOLLOW_UP_MESSAGE')
+        const cardTitle = requestAttributes.t('SKILL_NAME')
+    
+        let speakOutput = ''
+    
+        let inquiryTypeId = getResolvedSlotIDValue(request, 'infoTypeRequested')
+        if (!inquiryTypeId) {
+          inquiryTypeId = 'fullProfile'
+          speakOutput += requestAttributes.t('NOT_SURE_OF_TYPE_MESSAGE')
+        } else {
+          if (inquiryTypeId === 'emailAddress' || inquiryTypeId === 'fullProfile') {
+            speakOutput += requestAttributes.t('REPORT_EMAIL_ADDRESS', sessionAttributes.emailAddress)
+          }
+    
+          if (inquiryTypeId === 'userName' || inquiryTypeId === 'fullProfile') {
+            speakOutput += requestAttributes.t('REPORT_USERNAME', sessionAttributes.userName)
+          }
+        }
+    
+        speakOutput += repromptOutput
+    
+        return handlerInput.responseBuilder.
+          speak(speakOutput).
+          reprompt(repromptOutput).
+          withSimpleCard(cardTitle, speakOutput).
+          getResponse()
+      }
+}
+
 const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -149,7 +207,8 @@ exports.handler = Alexa.SkillBuilders.custom()
         CancelAndStopIntentHandler,
         FallbackIntentHandler,
         SessionEndedRequestHandler,
-        IntentReflectorHandler)
+        IntentReflectorHandler),
+        RequestInfoHandler
     .addErrorHandlers(
         ErrorHandler)
     .withCustomUserAgent('sample/hello-world/v1.2')

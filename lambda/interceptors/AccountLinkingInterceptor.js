d77789125d99565b38c130affca581e51fae5735
camila santacuz <milalovechaton@gmail.com>
2024-07-19 19:40:17 -0300

    add new logsx8

--- lambda/interceptors/AccountLinkingInterceptor.js
+++ lambda/interceptors/AccountLinkingInterceptor.js
@@ -3,14 +3,16 @@ const log = require("../lib/log");
 
 const AccountLinkingInterceptor = {
     async process(handlerInput) {
-        log.info('AccountLinkingInterceptor', handlerInput.requestEnvelope);
+        log.info('AccountLinkingInterceptor:::', handlerInput.requestEnvelope);
 
       const { request } = handlerInput.requestEnvelope;
+      log.info('AccountLinkingInterceptor:::', request);
       if (request.type === 'AlexaSkillEvent.ProvideLinkToken') {
         try {
           // Handle successful account linking
           const { accessToken, refreshToken } = await handleAccountLinking(request.linkToken);
           // Store tokens securely and update session attributes
+          log.info('AccountLinkingInterceptor:::', accessToken, refreshToken);
         } catch (error) {
           // Handle account linking failure
           console.error('Account linking failed:', error);
@@ -26,4 +28,5 @@ const AccountLinkingInterceptor = {
     // Exchange the link token for access and refresh tokens
     // using your authentication service (e.g., Amazon Cognito)
     // and return the tokens
+    log.info('AccountLinkingInterceptor:::', linkToken);
   }

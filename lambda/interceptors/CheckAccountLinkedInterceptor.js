a13438034a6b454115d5646053367e83230b9d45
camila santacuz <milalovechaton@gmail.com>
2024-07-21 16:25:14 -0300

    delete unwanted node_modulesx8

--- lambda/interceptors/CheckAccountLinkedInterceptor.js
+++ lambda/interceptors/CheckAccountLinkedInterceptor.js
@@ -1,31 +1,8 @@
 
-
-
 const CheckAccountLinkedInterceptor = {
     process(handlerInput) {
-        console.log("CheckAccountLinkedInterceptor");
-        const accessToken = handlerInput.requestEnvelope.session.user.accessToken;
-
-        if (!accessToken) {
-            console.log("CheckAccountLinkedInterceptor::: no token");
-           
-            const userPoolId ="us-east-1_MHOLeGbES"; // Replace with your user pool ID
-            const clientId = "odrahfu5lgbutk3p6p18e8bgu"
-            return handlerInput.responseBuilder
-                .addDirectivePayload({
-                    type: 'Connections.SendRequest',
-                    token:  'static-test-token',
-                    request: {
-                        type: 'ConnectionRequest',
-                        name: 'Lucia te cuida',
-                        payload: {
-                            userPoolId: userPoolId,
-                            clientId: clientId
-                        }
-                    }
-                })
-                .getResponse();
-        }
+        console.log("CheckAccountLinkedInterceptor:::", handlerInput.requestEnvelope.request.type);
+        
     }
 };
 

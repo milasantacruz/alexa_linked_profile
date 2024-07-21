b2031677a2d306b02c1501d20eecb69b0503be14
camila santacuz <milalovechaton@gmail.com>
2024-07-21 15:39:31 -0300

    delete unwanted node_modulesx5

--- lambda/interceptors/CheckAccountLinkedInterceptor.js
+++ lambda/interceptors/CheckAccountLinkedInterceptor.js
@@ -3,16 +3,16 @@ const { v4: uuidv4 } = require('uuid');
 
 
 const CheckAccountLinkedInterceptor = {
-    async process(handlerInput) {
+    process(handlerInput) {
         console.log("CheckAccountLinkedInterceptor");
         const accessToken = handlerInput.requestEnvelope.session.user.accessToken;
 
         if (!accessToken) {
             console.log("CheckAccountLinkedInterceptor::: no token");
             const token = uuidv4();
-            const userPoolId ="us-east-1_MHOLeGbES"; // Replace with your user pool ID
-            const clientId = "odrahfu5lgbutk3p6p18e8bgu"
-            const response = handlerInput.responseBuilder
+            const userPoolId = "us-east-1_MHOLeGbES"; // Replace with your user pool ID
+            const clientId = "XXXXXXXXXXXXXXXXXXXXXXXXX";
+            return handlerInput.responseBuilder
                 .addDirectivePayload({
                     type: 'Connections.SendRequest',
                     token: token,
@@ -26,8 +26,6 @@ const CheckAccountLinkedInterceptor = {
                     }
                 })
                 .getResponse();
-
-            return Promise.resolve(response);
         }
     }
 };

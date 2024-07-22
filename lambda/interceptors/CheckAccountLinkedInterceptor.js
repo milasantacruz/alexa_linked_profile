cd1e4999d3c83939e45a1217ed0007bd02293963
camila santacuz <milalovechaton@gmail.com>
2024-07-22 17:01:41 -0300

    testing CheckAccountx23

--- lambda/interceptors/CheckAccountLinkedInterceptor.js
+++ lambda/interceptors/CheckAccountLinkedInterceptor.js
@@ -42,7 +42,7 @@ const CheckAccountLinkedInterceptor = {
                         }
                     }
                 })
-                .withShouldEndSession(false)
+                .withShouldEndSession(true)
                 .getResponse();
         }
     }

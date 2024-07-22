d34884fc6689342b0d9a76b50020891870727bbd
camila santacuz <milalovechaton@gmail.com>
2024-07-22 15:58:53 -0300

    testing CheckAccountx9

--- lambda/interceptors/CheckAccountLinkedInterceptor.js
+++ lambda/interceptors/CheckAccountLinkedInterceptor.js
@@ -1,6 +1,19 @@
 const { v4: uuidv4 } = require('uuid');
 const log = require('../lib/log')
 
+/*
+The 
+addDirectivePayload
+ method is used to add a directive to the response sent 
+ back to the Alexa service. Directives are instructions that tell 
+ the Alexa service to perform specific actions, such as displaying 
+ a card or opening a web browser.
+*/
+
+/*
+withLinkAccountCard is a specific directive used to display an account 
+linking card for third-party services.
+*/
 
 const CheckAccountLinkedInterceptor = {
     async process(handlerInput) {
@@ -14,11 +27,12 @@ const CheckAccountLinkedInterceptor = {
             const clientId = "odrahfu5lgbutk3p6p18e8bgu"
             log.info("CheckAccountLinkedInterceptor:::NOTOKEN", token);
 
+            //http://ask-sdk-node-typedoc.s3-website-us-east-1.amazonaws.com/interfaces/interfaces.connections.sendrequestdirective.html
             return handlerInput.responseBuilder
-                .addDirectivePayload({
+                .addDirective({
                     type: 'Connections.SendRequest',
                     token,
-                    request: {
+                    payload: {
                         type: 'ConnectionRequest',
                         name: 'Lucia te cuida',
                         payload: {

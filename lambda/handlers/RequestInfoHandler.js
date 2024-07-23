21d4850e512ff0b8b5ede28834eb341ca49fd4b1
camila santacuz <milalovechaton@gmail.com>
2024-07-22 23:34:00 -0300

    testing CheckAccountx35

--- lambda/handlers/RequestInfoHandler.js
+++ lambda/handlers/RequestInfoHandler.js
@@ -1,16 +1,16 @@
-import { CognitoIdentityServiceProvider } from 'aws-sdk';
-import { info } from '../lib/log';
+const AWS = require('aws-sdk');
+const log = require('../lib/log')
 const RequestInfoHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
         && handlerInput.requestEnvelope.request.intent.name === 'RequestInfo';
     },
     async handle(handlerInput) {
-        info("reqInfoHandler:::");
+        log.info("reqInfoHandler:::");
         const accessToken = handlerInput.requestEnvelope.context.System.user.accessToken;
         
         if (!accessToken) {
-            info("reqInfoHandler:::NOTOKEN");
+            log.info("reqInfoHandler:::NOTOKEN");
             return handlerInput.responseBuilder
                 .speak('Please link your account to use this skill.')
                 .withLinkAccountCard()
@@ -20,7 +20,7 @@ const RequestInfoHandler = {
         try {
             const userData = await getUserProfile(accessToken);
             const speechText = `Your profile name is ${userData.name} and your email is ${userData.email}.`;
-            info("reqInfoHandler:::", userData);
+            log.info("reqInfoHandler:::", userData);
             return handlerInput.responseBuilder
                 .speak(speechText)
                 .getResponse();
@@ -33,8 +33,8 @@ const RequestInfoHandler = {
 };
 
 async function getUserProfile(accessToken) {
-    info("getUserProfile:::", accessToken);
-    const cognito = new CognitoIdentityServiceProvider();
+    log.info("getUserProfile:::", accessToken);
+    const cognito = new AWS.CognitoIdentityServiceProvider();
     
     // Assuming accessToken is the token to get the user details
     const params = {
@@ -42,7 +42,7 @@ async function getUserProfile(accessToken) {
     };
 
     const user = await cognito.getUser(params).promise();
-    info("getUser:::", user);
+    log.info("getUser:::", user);
     const attributes = user.UserAttributes.reduce((acc, attr) => {
         acc[attr.Name] = attr.Value;
         return acc;
@@ -54,4 +54,4 @@ async function getUserProfile(accessToken) {
     };
 }
 
-export default RequestInfoHandler
+module.exports = RequestInfoHandler

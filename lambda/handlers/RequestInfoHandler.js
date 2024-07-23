c59fc5deba3acaac61e2d5d0a8faea21a0af3b02
camila santacuz <milalovechaton@gmail.com>
2024-07-22 22:28:52 -0300

    testing CheckAccountx31

--- lambda/handlers/RequestInfoHandler.js
+++ lambda/handlers/RequestInfoHandler.js
@@ -1,6 +1,6 @@
 const Alexa = require('ask-sdk-core');
 const AWS = require('aws-sdk');
-
+const log = require('../lib/log')
 const RequestInfoHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -19,7 +19,7 @@ const RequestInfoHandler = {
         try {
             const userData = await getUserProfile(accessToken);
             const speechText = `Your profile name is ${userData.name} and your email is ${userData.email}.`;
-            
+            log.info("reqInfoHandler:::", userData);
             return handlerInput.responseBuilder
                 .speak(speechText)
                 .getResponse();
@@ -32,6 +32,7 @@ const RequestInfoHandler = {
 }
 
 async function getUserProfile(accessToken) {
+    log.info("getUserProfile:::", accessToken);
     const cognito = new AWS.CognitoIdentityServiceProvider();
     
     // Assuming accessToken is the token to get the user details
@@ -40,7 +41,7 @@ async function getUserProfile(accessToken) {
     };
 
     const user = await cognito.getUser(params).promise();
-    
+    log.info("getUser:::", user);
     const attributes = user.UserAttributes.reduce((acc, attr) => {
         acc[attr.Name] = attr.Value;
         return acc;
